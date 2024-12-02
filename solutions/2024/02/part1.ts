12f100905306f0beacb597a465061fbcd09c83b2
Ian Hornik <yiliansource@gmail.com>
2024-12-02 13:46:58 +0100

    refactor

--- solutions/2024/02/part1.ts
+++ solutions/2024/02/part1.ts
@@ -2,7 +2,7 @@ export default function (input: string): number {
     return input
         .split("\n")
         .map((l) => l.split(" ").map(Number))
-        .map((l) => l.map((e, i) => l[i + 1] - e).slice(0, -1))
+        .map((l) => l.slice(1).map((e, i) => e - l[i]))
         .map((d) => d.every((e) => Math.abs(e) <= 3) && (d.every((e) => e > 0) || d.every((e) => e < 0)))
         .reduce((a, s) => a + Number(s), 0);
 }
--- solutions/2024/02/part2.ts
+++ solutions/2024/02/part2.ts
@@ -2,11 +2,12 @@ export default function (input: string): number {
     return input
         .split("\n")
         .map((l) => l.split(" ").map(Number))
-        .map((l) => [l.slice(), ...[...Array(l.length).keys()].map((i) => l.slice(0, i).concat(l.slice(i + 1)))])
+        .map((l) => [l.slice(), ...[...Array(l.length).keys()].map((i) => l.toSpliced(i, 1))])
         .map((p) =>
             p
-                .map((l) => l.map((e, i) => l[i + 1] - e).slice(0, -1))
-                .some((d) => d.every((e) => Math.abs(e) <= 3) && (d.every((e) => e > 0) || d.every((e) => e < 0)))
+
+                .map((l) => l.slice(1).map((e, i) => e - l[i]))
+                .some((d) => d.every((e) => e > 0 && e <= 3) || d.every((e) => e < 0 && e >= -3))
         )
         .reduce((a, s) => a + Number(s), 0);
 }
