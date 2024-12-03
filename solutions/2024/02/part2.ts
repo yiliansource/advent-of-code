2baa5e4773b595dacd7dc98425a86907e21e7ac5
Ian Hornik <yiliansource@gmail.com>
2024-12-03 12:16:41 +0100

    benched

--- solutions/2024/02/part2.ts
+++ solutions/2024/02/part2.ts
@@ -5,7 +5,6 @@ export default function (input: string): number {
         .map((l) => [l.slice(), ...[...Array(l.length).keys()].map((i) => l.toSpliced(i, 1))])
         .map((p) =>
             p
-
                 .map((l) => l.slice(1).map((e, i) => e - l[i]))
                 .some((d) => d.every((e) => e > 0 && e <= 3) || d.every((e) => e < 0 && e >= -3))
         )
