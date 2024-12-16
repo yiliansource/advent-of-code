6bd7d56e360a6e6cd9527a2c80e8b58369257c56
Ian Hornik <yiliansource@gmail.com>
2024-12-16 18:05:08 +0100

    16/12/2024

--- solutions/2024/16/part2.ts
+++ solutions/2024/16/part2.ts
@@ -9,81 +9,50 @@ export default function (input: string): number {
     const ex = maze[ey].indexOf("E");
     maze[ey][ex] = ".";
 
-    const stringifyCoord = (c: [number, number]) => c.join(",");
-    const parseCoord = (s: string) => s.split(",").map(Number) as [number, number];
+    const stringifyState = (c: [number, number, number]) => c.join(",");
+    const parseState = (s: string) => s.split(",").map(Number) as [number, number, number];
 
-    const coordToDir = ([cx, cy]: [number, number]) => {
-        if (cx === 1) return 0;
-        if (cy === -1) return 1;
-        if (cx === -1) return 2;
-        if (cy === 1) return 3;
-        return -1;
+    const dirToCoord = (dir: number): [number, number] => {
+        if (dir === 0) return [1, 0];
+        if (dir === 1) return [0, -1];
+        if (dir === 2) return [-1, 0];
+        if (dir === 3) return [0, 1];
+        throw new Error();
     };
+    const nmod = (m: number, n: number) => ((m % n) + n) % n;
 
     const dist = new Map<string, number>();
-    const facing = new Map<string, number>(); // 0=east, 1=north, ...
     const prev = new Map<string, string[]>();
 
-    const Q = new Set<string>();
-    const onBestPath = new Set<string>();
+    const Q: string[] = [];
+    Q.push(stringifyState([sx, sy, 0]));
+    dist.set(stringifyState([sx, sy, 0]), 0);
 
-    for (let y = 0; y < maze.length; y++) {
-        for (let x = 0; x < maze[y].length; x++) {
-            if (maze[y][x] === ".") {
-                const s = stringifyCoord([x, y]);
-                dist.set(s, Number.POSITIVE_INFINITY);
-                Q.add(s);
-            }
-        }
-    }
-
-    dist.set(stringifyCoord([sx, sy]), 0);
-    facing.set(stringifyCoord([sx, sy]), 0);
-
-    while (Q.size > 0) {
-        let u = Q.values().next().value!;
-        for (const q of Q.values()) {
-            if (dist.get(q)! < dist.get(u)!) {
-                u = q;
-            }
-        }
-        const [ux, uy] = parseCoord(u);
-        if (ux === ex && uy === ey) {
-            break;
-        }
-
-        Q.delete(u);
+    while (Q.length > 0) {
+        const u = Q.pop()!;
+        const [ux, uy, facing] = parseState(u);
+        const [fx, fy] = dirToCoord(facing);
+        const distanceToU = dist.get(u)!;
 
         const neighbours = [
-            [1, 0],
-            [0, -1],
-            [-1, 0],
-            [0, 1],
-        ]
-            .map(([dx, dy]) => [ux + dx, uy + dy] as [number, number])
-            .filter(([cx, cy]) => maze[cy]?.[cx] === ".")
-            .map(stringifyCoord);
+            [ux, uy, nmod(facing - 1, 4), distanceToU + 1000],
+            [ux, uy, nmod(facing + 1, 4), distanceToU + 1000],
+        ];
 
-        for (const v of neighbours) {
-            const [vx, vy] = parseCoord(v);
-            const dx = vx - ux;
-            const dy = vy - uy;
-            const dir = coordToDir([dx, dy]);
+        if (maze[uy + fy]?.[ux + fx] === ".") {
+            neighbours.unshift([ux + fx, uy + fy, facing, distanceToU + 1]);
+        }
 
-            const oldDist = dist.get(v) ?? Number.POSITIVE_INFINITY;
-            const newDist = dist.get(u)! + 1 + (dir !== facing.get(u) ? 1000 : 0);
-
-            if (v === "3,9") {
-                console.log(oldDist, newDist);
-            }
-
+        for (const [nx, ny, newFacing, newDist] of neighbours) {
+            const ns = stringifyState([nx, ny, newFacing]);
+            const oldDist = dist.get(ns) ?? Number.POSITIVE_INFINITY;
             if (newDist <= oldDist) {
-                dist.set(v, newDist);
-
+                dist.set(ns, newDist);
                 if (newDist < oldDist) {
-                    prev.set(v, [u]);
+                    prev.set(ns, [u]);
+                    if (!Q.includes(ns)) Q.push(ns);
                 } else {
-                    prev.set(v, [...(prev.get(v) ?? []), u]);
+                    prev.set(ns, [...(prev.get(ns) ?? []), u]);
                 }
             }
         }
@@ -91,29 +60,25 @@ export default function (input: string): number {
 
     const map = Array.from(Array(maze.length).keys()).map((i) => maze[i].slice());
 
-    for (const c of facing.keys()) {
-        const [cx, cy] = parseCoord(c);
-        map[cy][cx] = [">", "^", "<", "v"][facing.get(c)!];
-    }
+    const onBestPath = new Set<string>();
 
-    const stack = [stringifyCoord([ex, ey])];
+    const lastTileStates = Array.from(Array(4).keys()).map((d) => stringifyState([ex, ey, d]));
+    const minDist = Math.min(...lastTileStates.map((s) => dist.get(s) ?? Number.POSITIVE_INFINITY));
+
+    const stack = lastTileStates.filter((s) => dist.get(s) === minDist);
     while (stack.length > 0) {
         const t = stack.pop()!;
-        const [tx, ty] = parseCoord(t);
+        const [tx, ty, _] = parseState(t);
         map[ty][tx] = "O";
-        onBestPath.add(t);
+        onBestPath.add([tx, ty].join(","));
 
         const p = prev.get(t)!;
-        // console.log(p);
-        if (!p) {
-            break;
+        if (!!p) {
+            stack.push(...p);
         }
-
-        stack.push(...p);
     }
-
-    map[9][3] = "X";
-    console.log(map.map((l) => l.join("")).join("\n"));
 
     return onBestPath.size;
 }
+
+// this is horribly inefficient, but i'm not sure how to do this better without a full rewrite.
